d741a6059897eb55d12e8db5cb642765e0994a1d
Randy Truong <rtruong@u.northwestern.edu>
2024-12-01 18:27:49 -0600

    chore: tidied up flagOrSettingValue to follow conventions

--- roxctl/common/flags/env_flag.go
+++ roxctl/common/flags/env_flag.go
@@ -6,8 +6,9 @@ import (
 
 // flagOrSettingValue will either return the following:
 //   - the flag value, if the flag value is not the default value (i.e. flagChanged != false).
-//   - the setting's value, if the flag value is the default value (i.e. flag changed == false) _and_ the setting's value is non-empty.
-//   - the default value, if the flag value is the default value (i.e. flag changed != false) and the setting's value
+//   - the setting's value, if the flag value is the default value (i.e. flagChanged == false)
+//     _and_ the setting's value is non-empty.
+//   - the default value, if the flag value is the default value (i.e. flagChanged != false) and the setting's value
 //     is empty.
 func flagOrSettingValue(flagValue string, flagChanged bool, setting env.Setting) string {
 	if !flagChanged {
